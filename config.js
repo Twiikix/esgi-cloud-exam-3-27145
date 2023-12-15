2ce4da2e007ee5b7b9bf99d978570b99913ba18c
Twiikix <gregoire05.decrouez@gmail.com>
2023-12-15 11:19:36 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: '6WR5U0YFA8R98BNDD2IT', // TODO
+    secretAccessKey: 'BOvh1f0qWhsNgCmRllznBZ4nCjEP6maer92X3UJz', // TODO
+    endpoint: 's3.eu-central-1', // TODO
+    bucket: 'esgi-exam-cloud-bucket-27145', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
